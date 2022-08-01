49d4df48542dc093e9e667de7379590301b1f128
Francis Verissimo <francissv97@gmail.com>
2022-08-01 15:09:56 -0400

    use Firebase: data by Firestore

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,36 +1,41 @@
-import { FaGlobe, FaCode, FaGithub } from "react-icons/fa";
-import doctorCare from "../assets/projects/doctorCare.jpg";
-import feedbackWidget from "../assets/projects/feedbackWidget.jpg";
-import letMeAsk from "../assets/projects/letMeAsk.jpg";
-import devFinance from "../assets/projects/devFinance.jpg";
+import { useEffect, useState } from "react";
+import { db } from "../services/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { FaGlobe, FaGithub } from "react-icons/fa";
+
+interface ProjectsType {
+  id: number;
+  name: string;
+  imageURL: string;
+  githubRepoUrl: string;
+  applicationUrl?: string;
+  stack: string[];
+}
 
 export const Projects = () => {
-  const projects = [
-    {
-      id: 1,
-      src: feedbackWidget,
-      githubRepoUrl: "https://github.com/francissverissimo/feedback-widget",
-      applicationUrl: "https://feedback-widget-opal.vercel.app",
-    },
-    {
-      id: 2,
-      src: letMeAsk,
-      githubRepoUrl: "https://github.com/francissverissimo/letmeask",
-      applicationUrl: "https://letmeask-3b93b.firebaseapp.com",
-    },
-    {
-      id: 3,
-      src: doctorCare,
-      githubRepoUrl: "https://github.com/francissverissimo/doctorCare",
-      applicationUrl: "https://francissverissimo.github.io/doctorCare",
-    },
-    {
-      id: 4,
-      src: devFinance,
-      githubRepoUrl: "https://github.com/francissverissimo/dev-finances",
-      applicationUrl: "https://francissverissimo.github.io/dev-finances",
-    },
-  ];
+  const [projects, setProjets] = useState<ProjectsType[]>([]);
+
+  useEffect(() => {
+    const subscriber = () => {
+      async function getProjectsDoc() {
+        try {
+          const docRef = doc(db, "data-page", "projects");
+          const docSnap = await getDoc(docRef);
+
+          if (docSnap.exists()) {
+            const data = docSnap.data();
+            setProjets(data.projects);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      getProjectsDoc();
+    };
+
+    return subscriber();
+  }, []);
 
   return (
     <div
@@ -42,46 +47,48 @@ export const Projects = () => {
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Projetos
           </p>
-          <p className="py-6">Confira alguns dos meus trabalhos</p>
+          <p className="py-6 font-medium text-lg">
+            Confira alguns dos meus trabalhos
+          </p>
         </div>
 
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 sm:px-0">
-          {projects.map(({ id, src, applicationUrl, githubRepoUrl }) => (
-            <div
-              key={id}
-              className="flex flex-col shadow-md shadow-zinc-500 rounded-lg bg-zinc-800"
-            >
-              <img
-                src={src}
-                alt=""
-                className="rounded-md duration-200 mx-auto hover:scale-105 h-3/4"
-              />
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-8 sm:px-0">
+          {projects &&
+            projects.map(({ id, imageURL, applicationUrl, githubRepoUrl }) => (
+              <div key={id} className="flex flex-col shadow-lg shadow-zinc-500">
+                <img
+                  src={imageURL}
+                  alt=""
+                  className="duration-200 mx-auto hover:scale-105"
+                />
 
-              <div className="flex flex-col justify-start py-3">
-                <a
-                  href={applicationUrl}
-                  target="_blank"
-                  className="flex items-center w-full py-2 mx-4 duration-200 hover:text-orange-500 hover:cursor-pointer"
-                >
-                  <FaGlobe className="mr-1" />
-                  Aplicação
-                </a>
+                <div className="flex flex-col justify-start h-full py-3 bg-gradient-to-br from-zinc-900 to-zinc-600">
+                  {applicationUrl && (
+                    <a
+                      href={applicationUrl}
+                      target="_blank"
+                      className="flex items-center w-fit py-2 font-medium px-4 duration-200 hover:text-orange-500 hover:cursor-pointer"
+                    >
+                      <FaGlobe className="mr-1" />
+                      Demonstração
+                    </a>
+                  )}
 
-                <a
-                  href={githubRepoUrl}
-                  target="_blank"
-                  className="flex items-center w-full py-2 mx-4 duration-200 hover:text-orange-500 hover:cursor-pointer"
-                >
-                  <FaCode className="mr-1" />
-                  Repositório GitHub
-                </a>
+                  <a
+                    href={githubRepoUrl}
+                    target="_blank"
+                    className="flex items-center w-fit py-2 px-4 duration-200 hover:text-orange-500 hover:cursor-pointer"
+                  >
+                    <FaGithub className="mr-1" />
+                    Repositório GitHub
+                  </a>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
 
         <a
-          className="flex items-center text-md bg-zinc-800 rounded-lg p-2 mt-16 mx-auto duration-200 hover:scale-105 hover:text-orange-500"
+          className="flex items-center text-md bg-zinc-800 rounded-lg p-2 mt-16 mx-auto duration-200 hover:text-orange-500"
           href="https://github.com/francissverissimo"
           target="_blank"
         >
