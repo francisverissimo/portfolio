1df90b2d47b88edcb170da84c2c9dd9473bb50ea
Francis Verissimo <francissv97@gmail.com>
2022-08-01 22:22:59 -0400

    delete commented lines

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,6 +1,5 @@
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
-// import { BsFillPersonLinesFill } from "react-icons/bs";
 
 export const SocialLinks = () => {
   const links = [
@@ -35,17 +34,6 @@ export const SocialLinks = () => {
       href: "mailto:francissv97@gmail.com",
       style: "rounded-br-md",
     },
-    // {
-    //   id: 4,
-    //   child: (
-    //     <>
-    //       Resume <BsFillPersonLinesFill size={30} />
-    //     </>
-    //   ),
-    //   href: "https://curriculo-online.com",
-    //   style: "rounded-br-md",
-    //   download: true,
-    // },
   ];
 
   return (
@@ -54,10 +42,8 @@ export const SocialLinks = () => {
         {links.map(({ id, child, href, style }) => (
           <li
             key={id}
-            className={
-              `flex justify-between items-center w-40 h-14 px-3 ml-[-110px] hover:ml-[-10px] hover:rounded-md duration-300 bg-zinc-600 cursor-pointer
-              ${style}`
-            }
+            className={`flex justify-between items-center w-40 h-14 px-3 ml-[-110px] hover:ml-[-10px] hover:rounded-md duration-300 bg-zinc-600 cursor-pointer
+              ${style}`}
           >
             <a
               href={href}
