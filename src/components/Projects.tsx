a3cb84407f42c5ddcd9ffaa29cfa43b3a220a264
Francis Verissimo <francissv97@gmail.com>
2022-08-14 13:34:08 -0400

    adjust modal toggle behavior

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -82,85 +82,83 @@ export const Projects = () => {
       } text-white`}
     >
       {isLoading && <Loading />}
-
-      {showProjectModal ? (
+      {showProjectModal && (
         <ProjectModal
           closeModal={() => handleCloseModal()}
           projectToBeShown={projectToBeShownInTheModal}
         />
-      ) : (
-        <div
-          className={`max-w-screen-lg px-7 mx-auto flex flex-col justify-center w-full h-full ${
-            isLoading ? "hidden" : "visible"
-          }`}
-        >
-          <div className="pb-8">
-            <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-              {"Projetos"}
-            </p>
-            <p className="py-6 font-medium text-lg">
-              {"Alguns dos meus trabalhos"}
-            </p>
-          </div>
-
-          <div className="grid lg:grid-cols-2 gap-x-5 gap-y-8 sm:px-0">
-            {projects &&
-              projects.map(
-                ({ id, name, imageURL, applicationUrl, githubRepoUrl }) => (
-                  <div
-                    key={id}
-                    className="flex flex-col shadow-lg shadow-zinc-500"
-                  >
-                    <img
-                      src={imageURL}
-                      alt=""
-                      className="duration-200 mx-auto hover:scale-105 cursor-pointer"
-                      onClick={() => handleToggleModalProject(name)}
-                    />
-
-                    <div className="flex flex-col justify-start h-full py-3 bg-gradient-to-br from-zinc-900 to-zinc-600">
-                      <button
-                        className="font-sans font-medium w-fit mx-auto duration-200 hover:text-orange-500 hover:scale-105"
-                        onClick={() => handleToggleModalProject(name)}
-                      >
-                        {name}
-                      </button>
-
-                      {applicationUrl && (
-                        <a
-                          href={applicationUrl}
-                          target="_blank"
-                          className="flex items-center w-fit py-2 font-medium px-4 duration-200 hover:text-orange-500 hover:cursor-pointer"
-                        >
-                          <FaGlobe className="mr-1" />
-                          Demonstração
-                        </a>
-                      )}
-
-                      <a
-                        href={githubRepoUrl}
-                        target="_blank"
-                        className="flex items-center w-fit py-2 px-4 duration-200 hover:text-orange-500 hover:cursor-pointer"
-                      >
-                        <FaGithub className="mr-1" />
-                        Repositório GitHub
-                      </a>
-                    </div>
-                  </div>
-                )
-              )}
-          </div>
-
-          <a
-            className="flex items-center text-md bg-zinc-800 rounded-lg p-2 mt-16 mx-auto duration-200 hover:text-orange-500"
-            href="https://github.com/francissverissimo?tab=repositories"
-            target="_blank"
-          >
-            <FaGithub size={25} className="mr-2" />
-            Repositórios Github
-          </a>
-        </div>
       )}
+      <div
+        className={`max-w-screen-lg px-7 mx-auto flex flex-col justify-center w-full h-full ${
+          isLoading ? "hidden" : "visible"
+        }`}
+      >
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            {"Projetos"}
+          </p>
+          <p className="py-6 font-medium text-lg">
+            {"Alguns dos meus trabalhos"}
+          </p>
+        </div>
+
+        <div className="grid lg:grid-cols-2 gap-x-5 gap-y-8 sm:px-0">
+          {projects &&
+            projects.map(
+              ({ id, name, imageURL, applicationUrl, githubRepoUrl }) => (
+                <div
+                  key={id}
+                  className="flex flex-col shadow-lg shadow-zinc-500"
+                >
+                  <img
+                    src={imageURL}
+                    alt=""
+                    className="duration-200 mx-auto hover:scale-105 cursor-pointer"
+                    onClick={() => handleToggleModalProject(name)}
+                  />
+
+                  <div className="flex flex-col justify-start h-full py-3 bg-gradient-to-br from-zinc-900 to-zinc-600">
+                    <button
+                      className="font-sans font-medium w-fit mx-auto duration-200 hover:text-orange-500 hover:scale-105"
+                      onClick={() => handleToggleModalProject(name)}
+                    >
+                      {name}
+                    </button>
+
+                    {applicationUrl && (
+                      <a
+                        href={applicationUrl}
+                        target="_blank"
+                        className="flex items-center w-fit py-2 font-medium px-4 duration-200 hover:text-orange-500 hover:cursor-pointer"
+                      >
+                        <FaGlobe className="mr-1" />
+                        Demonstração
+                      </a>
+                    )}
+
+                    <a
+                      href={githubRepoUrl}
+                      target="_blank"
+                      className="flex items-center w-fit py-2 px-4 duration-200 hover:text-orange-500 hover:cursor-pointer"
+                    >
+                      <FaGithub className="mr-1" />
+                      Repositório GitHub
+                    </a>
+                  </div>
+                </div>
+              )
+            )}
+        </div>
+
+        <a
+          className="flex items-center text-md bg-zinc-800 rounded-lg p-2 mt-16 mx-auto duration-200 hover:text-orange-500"
+          href="https://github.com/francissverissimo?tab=repositories"
+          target="_blank"
+        >
+          <FaGithub size={25} className="mr-2" />
+          Repositórios Github
+        </a>
+      </div>
     </div>
   );
 };
