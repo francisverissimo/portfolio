92235b8c22e062af14e0fed06628c6837d271703
Francis Verissimo <francissv97@gmail.com>
2022-08-14 13:01:39 -0400

    stop using SocialLinks Component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { Knowledge } from "./components/Knowledge";
 import { Home } from "./components/Home";
 import { NavBar } from "./components/NavBar";
 import { Projects } from "./components/Projects";
-import { SocialLinks } from "./components/SocialLinks";
 
 export function App() {
   return (
@@ -15,7 +14,6 @@ export function App() {
       <Projects />
       <Knowledge />
       <Contact />
-      <SocialLinks />
     </>
   );
 }
