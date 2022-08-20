e59e3d8f9b1c708528caed48e0f2099250b7e36c
Francis Verissimo <francissv97@gmail.com>
2022-08-19 20:50:26 -0400

    use knowledge images locally

---
+++ src/assets/knowledge/index.ts
@@ -0,0 +1,80 @@
+import html from "./images/html.svg";
+import css from "./images/css.svg";
+import typescript from "./images/typescript.svg";
+import react from "./images/react.svg";
+import sass from "./images/sass.svg";
+import tailwindcss from "./images/tailwindcss.svg";
+import styledComponents from "./images/styledComponents.svg";
+import node from "./images/node.svg";
+import github from "./images/github.svg";
+import graphql from "./images/graphql.svg";
+import firebase from "./images/firebase.svg";
+
+export const knowledge = [
+  {
+    id: 1,
+    image: html,
+    name: "HTML",
+    style: "shadow-orange-500",
+  },
+  {
+    id: 2,
+    image: css,
+    name: "CSS",
+    style: "shadow-blue-500",
+  },
+  {
+    id: 3,
+    image: typescript,
+    name: "Typescript",
+    style: "shadow-blue-500",
+  },
+  {
+    id: 4,
+    image: react,
+    name: "React",
+    style: "shadow-cyan-400",
+  },
+  {
+    id: 5,
+    image: sass,
+    name: "Sass",
+    style: "shadow-pink-400",
+  },
+  {
+    id: 6,
+    image: tailwindcss,
+    name: "TailwindCSS",
+    style: "shadow-sky-400",
+  },
+  {
+    id: 7,
+    image: styledComponents,
+    name: "Styled Components",
+    style: "shadow-purple-500",
+  },
+  {
+    id: 8,
+    image: node,
+    name: "NodeJS",
+    style: "shadow-lime-300",
+  },
+  {
+    id: 9,
+    image: github,
+    name: "Github",
+    style: "shadow-white",
+  },
+  {
+    id: 10,
+    image: graphql,
+    name: "GraphQL",
+    style: "shadow-pink-500",
+  },
+  {
+    id: 11,
+    image: firebase,
+    name: "Firebase",
+    style: "shadow-orange-500",
+  },
+];
--- src/components/Knowledge.tsx
+++ src/components/Knowledge.tsx
@@ -1,84 +1,6 @@
-export const Knowledge = () => {
-  const knowledges = [
-    {
-      id: 1,
-      imageURL:
-        "https://firebasestorage.googleapis.com/v0/b/francis-s-portfolio.appspot.com/o/knowledge%2Fhtml.png?alt=media&token=fee7a5db-55ca-4b8f-9221-83cdcd7b54c7",
-      name: "HTML",
-      style: "shadow-orange-500",
-    },
-    {
-      id: 2,
-      imageURL:
-        "https://firebasestorage.googleapis.com/v0/b/francis-s-portfolio.appspot.com/o/knowledge%2Fcss.png?alt=media&token=fed2ba0a-3c47-4c0a-8947-9aa7cee4c734",
-      name: "CSS",
-      style: "shadow-blue-500",
-    },
-    {
-      id: 3,
-      imageURL:
-        "https://firebasestorage.googleapis.com/v0/b/francis-s-portfolio.appspot.com/o/knowledge%2Ftypescript.svg?alt=media&token=cd9604b8-5b4f-45d0-903e-0ad6eff8adb6",
-      name: "Typescript",
-      style: "shadow-blue-500",
-    },
-    {
-      id: 4,
-      imageURL:
-        "https://firebasestorage.googleapis.com/v0/b/francis-s-portfolio.appspot.com/o/knowledge%2Freact.png?alt=media&token=c1acde26-fd70-492a-8db1-26f52809ed14",
-      name: "React",
-      style: "shadow-cyan-400",
-    },
-    {
-      id: 5,
-      imageURL:
-        "https://firebasestorage.googleapis.com/v0/b/francis-s-portfolio.appspot.com/o/knowledge%2Fsass.svg?alt=media&token=9d016426-9f33-4af9-bba9-2d1c84f0572a",
-      name: "Sass",
-      style: "shadow-pink-400",
-    },
-    {
-      id: 6,
-      imageURL:
-        "https://firebasestorage.googleapis.com/v0/b/francis-s-portfolio.appspot.com/o/knowledge%2Ftailwind.png?alt=media&token=b0108a85-e5ba-455e-8814-92da8d0fc676",
-      name: "TailwindCSS",
-      style: "shadow-sky-400",
-    },
-    {
-      id: 7,
-      imageURL:
-        "https://firebasestorage.googleapis.com/v0/b/francis-s-portfolio.appspot.com/o/knowledge%2Fstyled-components.png?alt=media&token=9263ddd0-6878-4af2-8a7a-6e114bd060ea",
-      name: "Styled Components",
-      style: "shadow-purple-500",
-    },
-    {
-      id: 8,
-      imageURL:
-        "https://firebasestorage.googleapis.com/v0/b/francis-s-portfolio.appspot.com/o/knowledge%2Fnode.png?alt=media&token=ff691a09-ada2-493a-9c8a-f0735cb7b488",
-      name: "NodeJS",
-      style: "shadow-lime-300",
-    },
-    {
-      id: 9,
-      imageURL:
-        "https://firebasestorage.googleapis.com/v0/b/francis-s-portfolio.appspot.com/o/knowledge%2Fgithub.png?alt=media&token=00afbfbe-ab8b-43ed-b0b7-51b34cbc34cd",
-      name: "Github",
-      style: "shadow-white",
-    },
-    {
-      id: 10,
-      imageURL:
-        "https://firebasestorage.googleapis.com/v0/b/francis-s-portfolio.appspot.com/o/knowledge%2Fgraphql.svg?alt=media&token=1c24ae98-aa93-4c89-b5fa-b82a3e94b3a4",
-      name: "GraphQL",
-      style: "shadow-pink-500",
-    },
-    {
-      id: 11,
-      imageURL:
-        "https://firebasestorage.googleapis.com/v0/b/francis-s-portfolio.appspot.com/o/knowledge%2Ffirebase.svg?alt=media&token=ee30dadb-6883-4b57-9945-1c61a75c2dd4",
-      name: "Firebase",
-      style: "shadow-orange-500",
-    },
-  ];
+import { knowledge } from "../assets/knowledge";
 
+export const Knowledge = () => {
   return (
     <div
       id="knowledge"
@@ -92,13 +14,13 @@ export const Knowledge = () => {
         </div>
 
         <div className="grid grid-cols-2 md:grid-cols-3 gap-5 text-center md:px-0 md:gap-8">
-          {knowledges &&
-            knowledges.map(({ id, imageURL, name, style }) => (
+          {knowledge &&
+            knowledge.map(({ id, image, name, style }) => (
               <div
                 key={id}
                 className={`bg-gradient-to-b from-zinc-600 to-zinc-900 shadow-md hover:scale-105 duration-300 py-2 rounded-md ${style}`}
               >
-                <img src={imageURL} alt="" className="w-20 mx-auto" />
+                <img src={image} alt="" className="w-20 mx-auto" />
                 <p className="mt-4 text-xs sm:text-sm font-semibold">{name}</p>
               </div>
             ))}
