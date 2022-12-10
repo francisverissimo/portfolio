9501df77905d7f7b00ebfeae54c02d3b74a2ef8f
francissverissimo <francissv97@gmail.com>
2022-12-10 12:15:57 -0400

    loading done

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,21 +1,21 @@
-import { AiOutlineLoading3Quarters, AiOutlineLoading } from "react-icons/ai";
-import { MdOutlineCircle } from "react-icons/md";
+import { Circle, CircleDashed, CircleNotch } from "phosphor-react";
+
 
 export function Loading() {
   return (
     <div className="bg-gradient-to-t from-zinc-600 via-zinc-400 to-zinc-200 flex justify-center items-center h-screen w-full">
       <div className="flex justify-center gap-4">
-        <AiOutlineLoading3Quarters
+        <CircleNotch
           color="#71717a"
           className="w-24 h-24 animate-spin"
         />
 
         <div className="flex flex-col justify-between">
-          <MdOutlineCircle className="animate-ping" />
-          <MdOutlineCircle color="#f97316" className="animate-ping" />
+          <Circle className="animate-ping" />
+          <Circle color="#f97316" className="animate-ping" />
         </div>
 
-        <AiOutlineLoading color="#f97316" className="w-24 h-24 animate-spin" />
+        <CircleDashed color="#f97316" className="w-24 h-24 animate-spin" />
       </div>
     </div>
   );
