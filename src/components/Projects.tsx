309bf3248d310062291c958baebc10f80bd028c7
francissverissimo <francissv97@gmail.com>
2024-10-01 21:04:44 -0400

    refactor: update Projects section layout and improve text styling

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,27 +1,28 @@
-import { ProjectFirestoreData } from "../types";
-import { CardProject } from "./CardProject";
+import { ProjectFirestoreData } from '../types'
+import { CardProject } from './CardProject'
 
 interface ProjectsProps {
-  projectsData: ProjectFirestoreData[];
+  projectsData: ProjectFirestoreData[]
 }
 
 export function Projects({ projectsData }: ProjectsProps) {
   return (
-    <div id="projects" className="min-h-screen w-full bg-zinc-800 py-20 text-white">
+    <div id="projects" className="min-h-screen w-full bg-zinc-800 py-20 text-zinc-50">
       <div className="mx-auto flex h-full w-full max-w-screen-lg flex-col justify-center">
-        <div className="px-4">
-          <p className="inline border-b-4 border-orange-500 text-4xl font-bold">Projetos</p>
+        <div className="flex flex-col items-center px-4">
+          <p className="text-lg italic">Alguns dos meus últimos</p>
 
-          <p className="py-6 text-lg font-medium">Alguns dos meus trabalhos</p>
+          <p className="text-4xl font-bold uppercase">projetos</p>
         </div>
 
-        <div className="flex flex-col gap-x-5 gap-y-14">
-          {projectsData &&
-            projectsData
-              .filter((project) => project.active)
-              .map((project, index) => <CardProject key={project.id} index={index} project={project} />)}
+        <div className="flex flex-col gap-x-5 gap-y-14 pt-8">
+          {projectsData
+            .filter((project) => project.active)
+            .map((project, index) => (
+              <CardProject key={project.id} index={index} project={project} />
+            ))}
         </div>
       </div>
     </div>
-  );
+  )
 }
