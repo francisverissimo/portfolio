eaf1a6947cdd31ac41eb7e60c7f54fbda83c83ea
francissverissimo <francissv97@gmail.com>
2024-10-01 20:56:35 -0400

    refactor: enhance NavBar styling and improve mobile menu behavior

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,42 +1,48 @@
-import { useState } from "react";
-import { List, X } from "phosphor-react";
-import { Link } from "react-scroll";
+import { useState } from 'react'
+import { List, X } from 'phosphor-react'
+import { Link } from 'react-scroll'
 
 export function NavBar() {
-  const [showNav, setShowNav] = useState(false);
+  const [showNav, setShowNav] = useState(false)
 
   const links = [
     {
       id: 1,
-      name: "home",
-      link: "home",
-    },
-    {
-      id: 2,
-      link: "about",
-      name: "sobre",
+      name: 'home',
+      link: 'home',
     },
     {
       id: 3,
-      link: "projects",
-      name: "projetos",
+      link: 'projects',
+      name: 'projetos',
     },
-    // {
-    //   id: 4,
-    //   link: "knowledge",
-    //   name: "conhecimentos",
-    // },
+    {
+      id: 2,
+      link: 'about',
+      name: 'sobre',
+    },
     {
       id: 5,
-      link: "contact",
-      name: "contato",
+      link: 'contact',
+      name: 'contato',
     },
-  ];
+  ]
+
+  function handleClickMenuButton() {
+    if (!showNav) {
+      setShowNav(true)
+      document.body.style.overflow = 'hidden'
+      return
+    }
+
+    setShowNav(false)
+    document.body.style.overflow = 'auto'
+  }
 
   return (
-    <div className="fixed z-10 h-20 w-full bg-zinc-900">
-      <div className="mx-auto flex h-full max-w-screen-lg items-center justify-between px-4 text-white">
-        <h1 className="font-signature text-2xl md:text-2xl">Francis S. Verissimo</h1>
+    <div className="fixed z-[21] h-20 w-full">
+      <div className="relative mx-auto flex h-full max-w-screen-lg items-center justify-between text-zinc-50 backdrop-blur-md">
+        <strong className="p-4 text-2xl font-light italic">Francis Verissimo</strong>
 
         <ul className="hidden md:flex">
           {links.map(({ id, name, link }) => {
@@ -46,40 +52,50 @@ export function NavBar() {
                 key={id}
                 smooth
                 duration={500}
-                className="cursor-pointer px-4 py-4 font-medium capitalize text-zinc-400 duration-75 first:pl-0 last:pr-0 hover:scale-110"
+                className="cursor-pointer p-4 font-medium uppercase text-zinc-400 duration-75 hover:text-orange-500"
               >
                 {name}
               </Link>
-            );
+            )
           })}
         </ul>
 
         <div
-          onClick={() => setShowNav(!showNav)}
-          className="z-10 cursor-pointer py-4 pl-4 pr-0 text-zinc-400 md:hidden"
+          onClick={handleClickMenuButton}
+          className="absolute right-0 z-[24] cursor-pointer p-4 text-zinc-400 hover:text-orange-500 md:hidden"
         >
-          {showNav ? <X size={32} /> : <List size={32} />}
+          <List size={32} />
         </div>
-
-        {showNav && (
-          <ul className="absolute top-0 left-0 flex h-screen w-full flex-col items-center justify-center bg-gradient-to-b from-zinc-900 to-zinc-600 text-zinc-400">
-            {links.map(({ id, name, link }) => {
-              return (
-                <Link
-                  key={id}
-                  onClick={() => setShowNav(!showNav)}
-                  to={link}
-                  smooth
-                  duration={500}
-                  className="cursor-pointer px-4 py-6 text-4xl capitalize"
-                >
-                  {name}
-                </Link>
-              );
-            })}
-          </ul>
-        )}
       </div>
+
+      {showNav && (
+        <ul
+          style={{ backdropFilter: 'blur(10px)' }}
+          className="fixed left-0 top-0 z-[22] flex h-screen w-full flex-col items-center justify-center bg-zinc-950/50"
+        >
+          <div
+            onClick={handleClickMenuButton}
+            className="absolute right-0 top-0 z-[24] flex h-20 cursor-pointer items-center p-4 text-zinc-400 hover:text-orange-500"
+          >
+            <X size={32} />
+          </div>
+
+          {links.map(({ id, name, link }) => {
+            return (
+              <Link
+                key={id}
+                onClick={handleClickMenuButton}
+                to={link}
+                smooth
+                duration={500}
+                className="cursor-pointer p-4 text-4xl font-light uppercase text-zinc-50 transition hover:text-orange-500"
+              >
+                {name}
+              </Link>
+            )
+          })}
+        </ul>
+      )}
     </div>
-  );
+  )
 }
